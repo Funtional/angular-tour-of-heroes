8c86dff7115dc57fb91175445adc5201ff8c2435
liaodehe <rivernash@163.com>
2018-09-23 11:15:06 +0800

    你使用 CLI 创建了第二个组件 HeroesComponent。 你把 HeroesComponent 添加到了壳组件 AppComponent中，以便显示它。 你使用 UppercasePipe 来格式化英雄的名字。 你用 ngModel 指令实现了双向数据绑定。 你知道了 AppModule。 你把 FormsModule 导入了 AppModule，以便 Angular 能识别并应用 ngModel 指令。 你知道了把组件声明到 AppModule 是很重要的，并认识到 CLI 会自动帮你声明它。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
+// Angular 需要知道如何把应用程序的各个部分组合到一起，以及该应用需要哪些其它文件和库。 这些信息被称为元数据（metadata）。
+// 有些元数据位于 @Component 装饰器中，你会把它加到组件类上。 另一些关键性的元数据位于 @NgModule 装饰器中。
+import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
 
+// 注意 AppModule 声明了应用中的所有组件
+// 每个组件都必须声明在（且只能声明在）一个 NgModule 中。
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent // HeroesComponent 已经声明在了 @NgModule.declarations 数组中。
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // 把 FormsModule 添加到 @NgModule 元数据的 imports 数组中，这里是该应用所需外部模块的列表。
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {Hero} from './hero';
 
-@Component({
+@Component({ // @Component 是个装饰器函数，用于为该组件指定 Angular 所需的元数据。
+             //   selector— 组件的选择器（CSS 元素选择器）
+             //   templateUrl— 组件模板文件的位置。
+             //   styleUrls— 组件私有 CSS 样式表文件的位置。
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+// 始终要 export 这个组件类，以便在其它地方（比如 AppModule）导入它。
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() {
   }
 
+  // ngOnInit 是一个生命周期钩子，Angular 在创建完组件后很快就会调用 ngOnInit。这里是放置初始化逻辑的好地方。
   ngOnInit() {
   }
 
